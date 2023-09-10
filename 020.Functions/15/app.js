930fd65d0c4841b21e7d511b741bfc1d89589880
SinegubAnastasiya <fedukevich98@mail.ru>
2023-09-11 00:07:02 +0300

    add task 15

--- 020.Functions/15/app.js
+++ 020.Functions/15/app.js
@@ -4,3 +4,37 @@
 // произведения всех элементов массива. Если результат функции проверки – true,
 // то вызывать новую функцию, возвращающую произведение всех элементов
 // массива
+
+const n = 3;
+
+function checkArr(arr) {
+    let res = arr.every(function(elem) {
+        if (!isNaN(elem)) return true
+        else return false
+    })
+    return res
+}
+
+function pushArr(n) {
+    const arr = [];
+    for (let i = 0; i < n; i++) {
+        arr.push(prompt());
+    }
+    return arr
+}
+
+let arr = pushArr(n);
+
+function multiplyArr(arr) {
+    let check = checkArr(arr)
+    if (check === true) {
+        let res = arr.reduce((proizv, elem) => proizv * elem,1)
+        return res;
+    } else {
+        return 'ERROR'
+    }
+}
+
+let mult = multiplyArr(arr)
+console.log(mult);
+
