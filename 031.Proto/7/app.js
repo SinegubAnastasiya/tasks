a2c0b5ca9424f908cffa4056638116a552c6d62d
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-09 19:17:56 +0300

    added task 7

--- 031.Proto/7/app.js
+++ 031.Proto/7/app.js
@@ -4,3 +4,23 @@
 // (≥ 1); расстояние между столбами (10 – 30 метров); ширина столба (10 – 50
 // сантиметров). Рассчитайте расстояние между первым и последним столбом в
 // сантиметрах
+
+const btn = document.querySelector('button');
+const count = document.querySelector('.count');
+const dist = document.querySelector('.dist');
+const width = document.querySelector('.width');
+const p = document.querySelector('p');
+
+btn.addEventListener('click', () => {
+    try {
+        if(count.value < 1) throw new Error('количество столбов is invalid')
+        if(dist.value < 10 && dist.value > 30) throw new Error('расстояние между столбами is invalid')
+        if(width.value < 0.1 && width.value > 0.5) throw new Error('ширина столба is invalid')
+        let res = 0;
+        if (count.value == 1) res = dist.value
+        else res = (dist.value * (count.value - 1) + (count.value - 2) * width.value) * 100;
+        p.textContent = res
+    } catch (error) {
+        alert(error.message)
+    }
+})
