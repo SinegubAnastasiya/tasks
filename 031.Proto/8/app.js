31c7f2fd2e408835ec982b7a1011b687ceaf719f
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-11 20:02:25 +0300

    added task 8

--- 031.Proto/8/app.js
+++ 031.Proto/8/app.js
@@ -1,2 +1,22 @@
 // На вход программе подается число в двоичной системе счисления. Необходимо
 // преобразовать его в десятичную систему счисления
+
+const inp = document.querySelector('input')
+const btn = document.querySelector('button')
+const p = document.querySelector('p')
+let res = 0;
+
+btn.addEventListener('click', () => {
+    try {
+        if (!inp.value) throw new Error('Input is empty')
+        // res = parseInt(inp.value, 2)
+        for (let i = 0; i < inp.value.length; i++) {
+            res += +inp.value[i] * 2 ** (inp.value.length - i - 1)
+        }
+        console.log(typeof(res));
+        p.textContent = res
+        inp.value = ''
+    } catch (error) {
+        alert(error.message)
+    }
+})
