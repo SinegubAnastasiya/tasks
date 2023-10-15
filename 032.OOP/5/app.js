1550f469f92772120f71a9ba156433ead890d3e3
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-15 22:41:18 +0300

    added tasks 5, 8

--- 032.OOP/5/app.js
+++ 032.OOP/5/app.js
@@ -1,3 +1,86 @@
 // Реализуйте класс Anagram (Анаграмма это слово или фраза , полученный путем
 //     замены букв исходного слова или фразы). Создать функцию для вывода ряда true,
 //     если слова являются анаграммами. Добавить проверки на ввод
+
+// class Anagram {
+//     firstWord;
+//     secondWord;
+
+//     constructor (first, second) {
+//         this.firstWord = first;
+//         this.secondWord = second;
+//     }
+
+//     checkWord = () => {
+//         try {
+//             if (!this.firstWord || !this.secondWord) throw new Error('No words')
+//             if (!isNaN(this.firstWord) || !isNaN(this.secondWord)) throw new Error('It is not a string')
+//             if (this.firstWord.split('').sort().join('') == this.secondWord.split('').sort().join('')) return true
+//             else return false
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const anagram = new Anagram('пила', 'липа')
+// console.log(anagram.checkWord());
+
+// class Anagram {
+//     firstWord;
+//     secondWord;
+
+//     checkWord = () => {
+//         try {
+//             if (!this.firstWord || !this.secondWord) throw new Error('No words')
+//             if (!isNaN(this.firstWord) || !isNaN(this.secondWord)) throw new Error('It is not a string')
+//             if (this.firstWord.split('').sort().join('') == this.secondWord.split('').sort().join('')) return true
+//             else return false
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const anagram = new Anagram()
+// anagram.firstWord = 'пила';
+// anagram.secondWord = 'липа';
+// console.log(anagram.checkWord());
+
+// class Anagram {
+//     firstWord = 'пила';
+//     secondWord = 'липа';
+
+//     checkWord = () => {
+//         try {
+//             if (!this.firstWord || !this.secondWord) throw new Error('No words')
+//             if (!isNaN(this.firstWord) || !isNaN(this.secondWord)) throw new Error('It is not a string')
+//             if (this.firstWord.split('').sort().join('') == this.secondWord.split('').sort().join('')) return true
+//             else return false
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const anagram = new Anagram()
+// console.log(anagram.checkWord());
+
+class Anagram {
+    firstWord;
+    secondWord;
+
+    checkWord = (firstWord, secondWord) => {
+        try {
+            if (!firstWord || !secondWord) throw new Error('No words')
+            if (!isNaN(firstWord) || !isNaN(secondWord)) throw new Error('It is not a string')
+            if (firstWord.split('').sort().join('') == secondWord.split('').sort().join('')) return true
+            else return false
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+}
+
+const anagram = new Anagram()
+console.log(anagram.checkWord('пила', 'липа'));
--- 032.OOP/8/app.js
+++ 032.OOP/8/app.js
@@ -1,3 +1,45 @@
 // Реализуйте класс Validator. У него будет метод isEmail параметром принимает
 // строку и проверяет, является ли она корректным емейлом или нет. Если является
 // - возвращает true, если не является - то false.
+
+// class Validator {
+//     email;
+
+//     constructor (email) {
+//         this.email = email;
+//     }
+
+//     isEmail = () => {
+//         try {
+//             if (!this.email) throw new Error('Email is empty')
+//             if (!/^[\w]+@[a-z]+\.[a-z]{2,5}/gm.test(this.email)) throw new Error('Email is invalid')
+//             return true
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const validator = new Validator('fedukevi98@mail.ru');
+// console.log(validator.isEmail());
+
+class Validator {
+    email;
+
+    constructor (email) {
+        this.email = email;
+    }
+
+    isEmail = () => {
+        try {
+            if (!this.email) throw new Error('Email is empty')
+            if (!/^[\w]+@[a-z]+\.[a-z]{2,5}/gm.test(this.email)) throw new Error('Email is invalid')
+            return true
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+}
+
+const validator = new Validator('fedukevi98@mail.ru');
+console.log(validator.isEmail());
