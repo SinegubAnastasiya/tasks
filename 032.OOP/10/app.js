706abc59dba399ea47e24a522e3fbbff577ce63a
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-17 21:36:21 +0300

    added tasks 9, 10

--- 032.OOP/10/app.js
+++ 032.OOP/10/app.js
@@ -1,1 +1,68 @@
 // Дополнить класс Validator. Добавить метод isDate для проверки на дату.
+
+// class Validator {
+//     date;
+
+//     constructor (date) {
+//         this.date = date
+//     }
+
+//     isDate = () => {
+//         try {
+//             if (!/^[0-3][0-9]\.[0-1][0-9]\.[1-2][0-9][0-9][0-9]/gm.test(this.date)) throw new Error('Date is not valid')
+//             return true
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const validator = new Validator('17.10.2023')
+// console.log(validator.isDate());
+
+// class Validator {
+//     date = '17.10.2023';
+
+//     isDate = () => {
+//         try {
+//             if (!/^[0-3][0-9]\.[0-1][0-9]\.[1-2][0-9][0-9][0-9]/gm.test(this.date)) throw new Error('Date is not valid')
+//             return true
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const validator = new Validator()
+// console.log(validator.isDate());
+
+// class Validator {
+//     date;
+
+//     isDate = () => {
+//         try {
+//             if (!/^[0-3][0-9]\.[0-1][0-9]\.[1-2][0-9][0-9][0-9]/gm.test(this.date)) throw new Error('Date is not valid')
+//             return true
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const validator = new Validator()
+// validator.date = '17.10.2023'
+// console.log(validator.isDate());
+
+class Validator {
+    isDate = (date) => {
+        try {
+            if (!/^[0-3][0-9]\.[0-1][0-9]\.[1-2][0-9][0-9][0-9]/gm.test(date)) throw new Error('Date is not valid')
+            return true
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+}
+
+const validator = new Validator()
+console.log(validator.isDate('17.10.2023'));
--- 032.OOP/9/app.js
+++ 032.OOP/9/app.js
@@ -1,1 +1,69 @@
 // Дополнить класс Validator. Добавить метод isURL для проверки на url.
+
+// class Validator {
+//     url;
+
+//     constructor (url) {
+//         this.url = url;
+//     }
+
+//     isURL = () => {
+//         try {
+//             if(!/^[a-z]{4,5}:\/\/[\w\-]+\.[a-z]{2,5}/gm.test(this.url)) throw new Error('Url is invalid')
+//             return true
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const validator = new Validator('https://regex101.com/')
+// console.log(validator.isURL());
+
+// class Validator {
+//     url;
+
+//     isURL = () => {
+//         try {
+//             if(!/^[a-z]{4,5}:\/\/[\w\-]+\.[a-z]{2,5}/gm.test(this.url)) throw new Error('Url is invalid')
+//             return true
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const validator = new Validator()
+// validator.url = 'https://regex101.com/'
+// console.log(validator.isURL());
+
+// class Validator {
+//     url = 'https://regex101.com/';
+
+//     isURL = () => {
+//         try {
+//             if(!/^[a-z]{4,5}:\/\/[\w\-]+\.[a-z]{2,5}/gm.test(this.url)) throw new Error('Url is invalid')
+//             return true
+//         } catch (error) {
+//             console.log(error.message);
+//         }
+//     }
+// }
+
+// const validator = new Validator()
+// console.log(validator.isURL());
+
+class Validator {
+
+    isURL = (url) => {
+        try {
+            if(!/^[a-z]{4,5}:\/\/[\w\-]+\.[a-z]{2,5}/gm.test(url)) throw new Error('Url is invalid')
+            return true
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+}
+
+const validator = new Validator()
+console.log(validator.isURL('https://regex101.com/'));
