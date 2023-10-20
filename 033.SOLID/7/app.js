2ffeea45773fd6bd48fff6f165192fdad014c255
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-20 23:05:15 +0300

    added tasks 7-8

--- 033.SOLID/7/app.js
+++ 033.SOLID/7/app.js
@@ -14,3 +14,48 @@
 // }`
 // Необходимо осуществить удаление по id. Если совпадения нет – ошибка. Добавить
 // проверки
+
+class ServerDelete {
+    middleware (json) {
+        try {
+            if (!json.hasOwnProperty('id')) throw new Error ('Id does not exist')
+            const res = this.controller(json)
+            return res
+        } catch (error) {
+            return error.message;
+        }
+    }
+
+    controller (json) {
+        const res = this.service(json)
+        return res
+    }
+
+    service (json) {
+        const res = this.repository(json)
+        return res
+    }
+
+    repository (json) {
+        const arr = 
+        [
+            { "id": 1, "name": "Yesenia", "age": 22 },
+            { "id": 2, "name": "Hanna", "age": 22 },
+            { "id": 3, "name": "Stanislau", "age": 25 },
+            { "id": 4, "name": "German", "age": 18 },
+            { "id": 5, "name": "Maria", "age": 27 }
+        ]
+
+        const res =  arr.some((el) => {
+            if (el.id !== json.id) throw new Error ('ERROR')
+            else return true
+        })
+        if (res) {
+             return arr.filter(el => el.id !== json.id)
+        }
+    }
+}
+
+const serverdelete = new ServerDelete()
+const json = JSON.parse(`{ "id": 1 }`)
+console.log(serverdelete.middleware(json));
--- 033.SOLID/8/app.js
+++ 033.SOLID/8/app.js
@@ -2,3 +2,23 @@
 // кнопку. Класс содержит 1 метод валидации, называемый middleware, в котором
 // происходит проверка на uuid, введенный в input. Вывести true, если провека
 // успешна. 
+
+const btn = document.querySelector('button')
+const inp = document.querySelector('input')
+const p = document.querySelector('p')
+
+class DomHtml {
+    middleware = () => {
+        btn.addEventListener('click', () => {
+        try {
+                if(!/^[\da-z]{8}\-[\da-z]{4}\-[\da-z]{4}\-[\da-z]{4}\-[\da-z]{12}$/gm.test(inp.value)) throw new Error('Invalid')
+                else p.textContent = 'true'
+            }
+        catch (error) {
+            p.textContent = error.message;
+        }})
+    }
+}
+
+const domhtml = new DomHtml();
+domhtml.middleware();
