9d5656a0a118c3178b68386375ffc9170aff0cdb
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-22 20:16:38 +0300

    added some edits

--- 035.Polymorphism/6/app.js
+++ 035.Polymorphism/6/app.js
@@ -2,3 +2,25 @@
 // данные из 2 инпутов: почта и пароль и по клику на кнопку «отправить запрос на
 // сервер» (отправку запроса мы не проходили => просто вывести на экран
 // образовавщийся объект вида: {email: email, pwd: pwd}
+
+const btn = document.querySelector('button')
+const mail = document.querySelector('.mail')
+const pwd = document.querySelector('.pwd')
+const p = document.querySelector('p')
+
+class Client {
+    sendRequest() {
+        try {
+            btn.addEventListener('click', () => {
+                if (!/^[\w]+@[a-z]+\.[a-z]{2,5}$/gm.test(mail.value)) throw new Error ('Email is invalid')
+                if (!mail.value || !pwd.value) throw new Error ('Email or password is empty')
+                p.textContent = `Email: ${mail.value}, pwd: ${pwd.value}`
+            })
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+}
+
+const client = new Client();
+client.sendRequest()
