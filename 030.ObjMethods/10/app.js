446a30621582a19293fae5fb75eaa33cb51d9963
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-26 20:35:09 +0300

    added some tasks

--- 030.ObjMethods/10/app.js
+++ 030.ObjMethods/10/app.js
@@ -1,2 +1,13 @@
 // . У вас есть объект с информацией о пользователе. Извлеките имя и возраст
 // пользователя с использованием деструктуризации.
+
+const obj = {
+    name: 'Nastya',
+    age: 25,
+    city: 'Minsk',
+    country: 'Belarus'
+}
+
+const { name, age } = obj
+console.log(name);
+console.log(age);
