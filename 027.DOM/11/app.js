b6b7b64bc9f4d7cd3018842ae1fb5a3fdce935b6
SinegubAnastasiya <fedukevich98@mail.ru>
2023-10-29 20:42:09 +0300

    added task 11 in topic DOM

--- 027.DOM/11/app.js
+++ 027.DOM/11/app.js
@@ -1,1 +1,16 @@
 // По нажатию на кнопку поменять местами значения 2 инпутов
+
+const btn = document.querySelector('button')
+const first = document.querySelector('.first')
+const second = document.querySelector('.second')
+
+btn.addEventListener('click', () => {
+    try {
+        if(!first.value || !second.value) throw new Error ('Input is empty')
+        let res = first.value
+        first.value = second.value
+        second.value = res
+    } catch (error) {
+        alert(error.message)
+    }
+})
