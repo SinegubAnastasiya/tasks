5f9f332bdc6bf57eeca41a3ad68b069b1524677e
SinegubAnastasiya <fedukevich98@mail.ru>
2023-11-22 21:13:05 +0300

    added new task

--- 047.Singleton/2/app.js
+++ 047.Singleton/2/app.js
@@ -4,3 +4,38 @@
 // Необходимо также отображать только уникальные значения массива
 // Реализовать Html: input, button, div (для отображения результатов)
 // [1, 1, 4, 2, 3, 3] -> [4, 2]
+
+class Html {
+    countNumbers() {
+        const inp = document.querySelector('input');
+        const btn = document.querySelector('button');
+        const div = document.querySelector('div');
+        const p = document.querySelector('p');
+        const arr = [];
+        const arr_unique = [];
+
+        btn.addEventListener('click', () => {
+            for (let i = 0; i < inp.value.length; i++) {
+                arr.push(inp.value[i]);
+                inp.value = '';
+            }
+            div.innerHTML = arr;
+
+            for (let i = 0; i < arr.length; i++) {
+                if (!arr_unique.includes(arr[i]) && arr[i] % 2 == 0) {
+                    arr_unique.push(arr[i]);
+                }
+            }
+
+            // for (let i = 0; i < arr.length; i++) {
+            //     if (arr[i] !== arr[i+1] && arr[i] !== arr[i-1]) {
+            //         arr_unique.push(arr[i]);
+            //     }
+            // }
+            p.innerHTML = arr_unique;
+        })
+    }
+}
+
+const html = new Html();
+html.countNumbers();
--- 047.Singleton/3/app.js
+++ 047.Singleton/3/app.js
@@ -1,3 +1,20 @@
 // Вы вводите строку. Неоходимо проверить является ли эта строка датой и
 // соответствует шаблону вида хх-хх-хххх. Реализовать Html: input, button, div (для
 // отображения результата проверки регулярного выражения)
+
+class Html {
+    isDate() {
+        const inp = document.querySelector('input');
+        const btn = document.querySelector('button');
+        const div = document.querySelector('div');
+
+        btn.addEventListener('click', () => {
+            (/^[0-3][0-9]\-[1-12]{2}\-[1-2][0-9]{3}$/gm.test(inp.value)) ? 
+            div.innerHTML = 'true' : 
+            div.innerHTML = 'false'
+        })
+    }
+}
+
+const html = new Html();
+html.isDate();
