a1ede4eeffa4101ebaa40afe28b218e49d9a5fe3
SinegubAnastasiya <fedukevich98@mail.ru>
2023-11-23 21:17:27 +0300

    added new task

--- 047.Singleton/4/app.js
+++ 047.Singleton/4/app.js
@@ -1,2 +1,19 @@
 // 4. У вас есть 3 инпута. Необходимо вывести в div результат конкатенацию всех
 // инпутов по клику на кнопку
+
+class Html {
+    sumInp() {
+        const first = document.querySelector('.first');
+        const sec = document.querySelector('.second');
+        const third = document.querySelector('.third');
+        const btn = document.querySelector('button');
+        const div = document.querySelector('div');
+
+        btn.addEventListener('click', () => {
+            div.innerHTML += `${first.value}${sec.value}${third.value}`
+        })
+    }
+}
+
+const html = new Html();
+html.sumInp();
