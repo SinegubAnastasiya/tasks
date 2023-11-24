e7df556246f5869909cc0833b63e292ba2d2c39d
SinegubAnastasiya <fedukevich98@mail.ru>
2023-11-24 21:22:43 +0300

    added new task

--- 047.Singleton/5/app.js
+++ 047.Singleton/5/app.js
@@ -1,2 +1,19 @@
 // 5. При наведении курсора на изображение, необходимо осуществить замену этого
 // изображения на другое.
+
+class Html{
+    changeImg() {
+        const img = document.querySelector('img');
+        
+        img.addEventListener('mouseover', () => {
+            img.src = "./assets/img2.jpeg";
+        })
+        
+        img.addEventListener('mouseout', () => {
+            img.src = "./assets/img1.png";
+        })
+    }
+}
+
+const html = new Html();
+html.changeImg();
